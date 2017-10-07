28fcc0668cfd63473e53edecebb21d2064a7f208
itstathagatasarkar <32410605+itstathagatasarkar@users.noreply.github.com>
2017-10-07 11:27:40 +0530

    Add files via upload

---
+++ BBMS.Exceptions/BloodBankException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBMS.Exceptions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BloodBankException: ApplicationException
+    {
+        public BloodBankException()
+            :base()
+        {
+
+        }
+        public BloodBankException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
